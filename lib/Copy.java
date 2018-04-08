cdcb284fc3bc3f19f863cd06432032b5da7806d1
carollqp <li005403@stthomas.edu>
2018-04-07 21:15:34 -0500

    Add UML class diagram

---
+++ lib/Copy.java
@@ -0,0 +1,11 @@
+
+public class Copy {
+	private String title = "";
+	private String barcode = "";
+
+	public Copy(String title, String barcode) {
+		this.title = title;
+		this.barcode = barcode;
+
+	}
+}
